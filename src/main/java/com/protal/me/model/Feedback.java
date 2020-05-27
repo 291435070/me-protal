67e94dc6ed4205f4c6598377f6c4c89a73c8518d
291435070@qq.com <Liu198771>
2020-05-27 11:45:23 +0800

    ...

--- src/main/java/com/protal/me/model/Feedback.java
+++ src/main/java/com/protal/me/model/Feedback.java
@@ -1,73 +1,73 @@
 package com.protal.me.model;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class Feedback {
 
-	private Long id;
+    private Long id;
 
-	@NotNull(message = "类型不能为空")
-	private Integer type;
+    @NotNull(message = "类型不能为空")
+    private Integer type;
 
-	@NotBlank(message = "内容不能为空")
-	@Length(max = 100, message = "内容长度不能超过100")
-	private String content;
+    @NotBlank(message = "内容不能为空")
+    @Length(max = 100, message = "内容长度不能超过100")
+    private String content;
 
-	@NotBlank(message = "邮箱不能为空")
-	@Email(message = "邮箱格式不不正确")
-	private String email;
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不不正确")
+    private String email;
 
-	private String date;
+    private String date;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Integer getType() {
-		return type;
-	}
+    public Integer getType() {
+        return type;
+    }
 
-	public void setType(Integer type) {
-		this.type = type;
-	}
+    public void setType(Integer type) {
+        this.type = type;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-	}
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
 
 }
