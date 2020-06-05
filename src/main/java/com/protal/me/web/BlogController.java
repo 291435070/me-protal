cc361202a070f546c8937a7c5f9d9e06a0b5f7de
291435070@qq.com <James>
2020-06-05 18:46:31 +0800

    add blog html

--- src/main/java/com/protal/me/web/BlogController.java
+++ src/main/java/com/protal/me/web/BlogController.java
@@ -1,9 +1,9 @@
 package com.protal.me.web;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.google.common.base.CharMatcher;
+import com.google.gson.Gson;
+import com.protal.me.dao.BlogRepository;
+import com.protal.me.model.Blog;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.util.CollectionUtils;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -29,16 +29,11 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.google.common.base.CharMatcher;
-import com.protal.me.dao.BlogRepository;
-import com.protal.me.model.Blog;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <pre>1.ES下载，https://www.elastic.co/cn/downloads/elasticsearch，选择对应版本；</pre>
@@ -52,157 +47,159 @@ import com.protal.me.model.Blog;
 @RequestMapping("blog")
 public class BlogController {
 
-	private static final Logger LOG = LoggerFactory.getLogger(BlogController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(BlogController.class);
 
-	@Autowired
-	private BlogRepository blogRepository;
+    @Autowired
+    private BlogRepository blogRepository;
 
-	@Autowired
-	private ElasticsearchTemplate elasticsearchTemplate;
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
 
-	@PostMapping("insert")
-	public Object insert(@RequestBody Blog blog) {
-		blog.setDate(LocalDateTime.now().toString());
-		LOG.info(blog.toString());
-		return blogRepository.save(blog);
-	}
+    @PostMapping("insert")
+    public Object insert(@RequestBody Blog blog) {
+        blog.setDate(LocalDateTime.now().toString());
+        LOG.info(blog.toString());
+        return blogRepository.save(blog);
+    }
 
-	@PostMapping("delete")
-	public Object delete(@RequestBody Blog blog) {
-		LOG.info(blog.toString());
-		blogRepository.delete(blog);
-		return blog;
-	}
+    @PostMapping("delete")
+    public Object delete(@RequestBody Blog blog) {
+        LOG.info(blog.toString());
+        blogRepository.delete(blog);
+        return blog;
+    }
 
-	@PostMapping("update")
-	public Object update(@RequestBody Blog blog) {
-		LOG.info(blog.toString());
-		Assert.notNull(blog.getId(), "Cannot update 'null' id.");
-		return blogRepository.save(blog);
-	}
+    @PostMapping("update")
+    public Object update(@RequestBody Blog blog) {
+        LOG.info(blog.toString());
+        Assert.notNull(blog.getId(), "Cannot update 'null' id.");
+        return blogRepository.save(blog);
+    }
 
-	@GetMapping("select")
-	public Object select(@RequestBody Blog blog) {
-		LOG.info(blog.toString());
-		Assert.notNull(blog.getId(), "Cannot select 'null' id.");
-		return blogRepository.findById(blog.getId()).get();
-	}
+    @GetMapping("select")
+    public Object select(@RequestBody Blog blog) {
+        LOG.info(blog.toString());
+        Assert.notNull(blog.getId(), "Cannot select 'null' id.");
+        return blogRepository.findById(blog.getId()).get();
+    }
 
-	@GetMapping("get/all")
-	public Object getAll() {
-		// return blogRepository.search(QueryBuilders.matchAllQuery());
-		return blogRepository.findAll();
-	}
+    @GetMapping("get/all")
+    public Object getAll(@PageableDefault Pageable pageable) {
+        LOG.info("{}", new Gson().toJson(pageable));
+        // return blogRepository.search(QueryBuilders.matchAllQuery());
+        return blogRepository.findAll(pageable).getContent();
+    }
 
-	@SuppressWarnings("deprecation")
-	@PostMapping("save/batch")
-	public Object saveBatch() throws Exception {
-		Blog blog = null;
-		for (int i = 1; i <= 650; i++) {
-			LOG.info("第------{}------页", i);
-			Document doc = Jsoup.connect("https://www.jqhtml.com/category/java/page/" + i).get();
-			Element list = doc.getElementsByClass("content-list").first();
-			Elements item = list.getElementsByTag("li");
-			for (Element ele : item) {
-				Element e = ele.getElementsByTag("a").first();
-				LOG.info("{}->{}", e.attr("href"), e.html());
-				blog = new Blog();
-				blog.setId(Long.parseLong(CharMatcher.javaDigit().retainFrom(e.attr("href"))));
-				blog.setTitle(e.html());
-				blog.setContent(e.attr("href"));
-				blog.setDate(LocalDateTime.now().toString());
-				blogRepository.save(blog);
-			}
-		}
-		return blogRepository.count();
-	}
+    // 批量导入数据到ES
+    @SuppressWarnings("deprecation")
+    @PostMapping("save/batch")
+    public Object saveBatch() throws Exception {
+        Blog blog = null;
+        for (int i = 1; i <= 650; i++) {
+            LOG.info("第------{}------页", i);
+            Document doc = Jsoup.connect("https://www.jqhtml.com/category/java/page/" + i).get();
+            Element list = doc.getElementsByClass("content-list").first();
+            Elements item = list.getElementsByTag("li");
+            for (Element ele : item) {
+                Element e = ele.getElementsByTag("a").first();
+                LOG.info("{}->{}", e.attr("href"), e.html());
+                blog = new Blog();
+                blog.setId(Long.parseLong(CharMatcher.javaDigit().retainFrom(e.attr("href"))));
+                blog.setTitle(e.html());
+                blog.setContent(e.attr("href"));
+                blog.setDate(LocalDateTime.now().toString());
+                blogRepository.save(blog);
+            }
+        }
+        return blogRepository.count();
+    }
 
-	// 精确查询，指定字段
-	@GetMapping("query/term")
-	public Object queryTerm(@RequestParam String title, @PageableDefault Pageable pageable) {
-		LOG.info(title);
+    // 精确查询，指定字段
+    @GetMapping("query/term")
+    public Object queryTerm(@RequestParam String title, @PageableDefault Pageable pageable) {
+        LOG.info(title);
 
-		NativeSearchQuery query = new NativeSearchQueryBuilder()
-				.withQuery(QueryBuilders.termQuery("title.keyword", title)).withPageable(pageable)
-				.withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC)).build();
-		List<Blog> list = elasticsearchTemplate.queryForList(query, Blog.class);
+        NativeSearchQuery query = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.termQuery("title.keyword", title)).withPageable(pageable)
+                .withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC)).build();
+        List<Blog> list = elasticsearchTemplate.queryForList(query, Blog.class);
 
-		return list;
-	}
+        return list;
+    }
 
-	// 分词查询，指定字段
-	@GetMapping("query/match")
-	public Object queryMatch(@RequestParam String title, @PageableDefault Pageable pageable) {
-		LOG.info(title);
+    // 分词查询，指定字段
+    @GetMapping("query/match")
+    public Object queryMatch(@RequestParam String title, @PageableDefault Pageable pageable) {
+        LOG.info(title);
 
-		NativeSearchQuery query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.matchQuery("title", title))
-				.withPageable(pageable).withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC)).build();
-		List<Blog> list = elasticsearchTemplate.queryForList(query, Blog.class);
+        NativeSearchQuery query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.matchQuery("title", title))
+                .withPageable(pageable).withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC)).build();
+        List<Blog> list = elasticsearchTemplate.queryForList(query, Blog.class);
 
-		return list;
-	}
+        return list;
+    }
 
-	// 分词查询，不指定字段，即所有字段
-	@GetMapping("query/string")
-	public Object queryString(@RequestParam String title, @PageableDefault Pageable pageable) {
-		LOG.info(title);
+    // 分词查询，不指定字段，即所有字段
+    @GetMapping("query/string")
+    public Object queryString(@RequestParam String title, @PageableDefault Pageable pageable) {
+        LOG.info(title);
 
-		NativeSearchQuery query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.queryStringQuery(title))
-				.withPageable(pageable).withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC)).build();
-		List<Blog> list = elasticsearchTemplate.queryForList(query, Blog.class);
+        NativeSearchQuery query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.queryStringQuery(title))
+                .withPageable(pageable).withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC)).build();
+        List<Blog> list = elasticsearchTemplate.queryForList(query, Blog.class);
 
-		return list;
-	}
+        return list;
+    }
 
-	// 分词查询，指定字段、高亮显示
-	@GetMapping("query/highlight")
-	public Object queryHighlight(@RequestParam String title, @PageableDefault Pageable pageable) {
-		LOG.info(title);
-		// 高亮设置
-		Field highlight = new HighlightBuilder.Field("title").preTags("<font style='color:red'>").postTags("</font>");
+    // 分词查询，指定字段、高亮显示
+    @GetMapping("query/highlight")
+    public Object queryHighlight(@RequestParam String title, @PageableDefault Pageable pageable) {
+        LOG.info(title);
+        // 高亮设置
+        Field highlight = new HighlightBuilder.Field("title").preTags("<font style='color:red'>").postTags("</font>");
 
-		NativeSearchQuery query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.matchQuery("title", title))
-				.withPageable(pageable).withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC))
-				.withHighlightFields(highlight).build();
+        NativeSearchQuery query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.matchQuery("title", title))
+                .withPageable(pageable).withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC))
+                .withHighlightFields(highlight).build();
 
-		AggregatedPage<Blog> aggregatedPage = elasticsearchTemplate.queryForPage(query, Blog.class,
-				new SearchResultMapper() {
-					@Override
-					public <T> T mapSearchHit(SearchHit searchHit, Class<T> type) {
-						return null;
-					}
+        AggregatedPage<Blog> aggregatedPage = elasticsearchTemplate.queryForPage(query, Blog.class,
+                new SearchResultMapper() {
+                    @Override
+                    public <T> T mapSearchHit(SearchHit searchHit, Class<T> type) {
+                        return null;
+                    }
 
-					@SuppressWarnings("unchecked")
-					@Override
-					public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz,
-							Pageable pageable) {
-						LOG.info("总数----------{}", response.getHits().getTotalHits());
-						if (CollectionUtils.isEmpty(response.getHits().getHits())) {
-							return null;
-						}
-						List<Blog> blogs = new ArrayList<>();
-						Blog blog = null;
-						for (SearchHit hit : response.getHits()) {
-							blog = new Blog();
-							blog.setId(Long.parseLong(hit.getSourceAsMap().get("id").toString()));
-							blog.setContent(hit.getSourceAsMap().get("content").toString());
-							blog.setDate(hit.getSourceAsMap().get("date").toString());
-							HighlightField title = hit.getHighlightFields().get("title");
-							if (null != title) {
-								blog.setTitle(title.fragments()[0].toString());
-							}
-							blogs.add(blog);
-						}
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz,
+                                                            Pageable pageable) {
+                        LOG.info("总数----------{}", response.getHits().getTotalHits());
+                        if (CollectionUtils.isEmpty(response.getHits().getHits())) {
+                            return null;
+                        }
+                        List<Blog> blogs = new ArrayList<>();
+                        Blog blog = null;
+                        for (SearchHit hit : response.getHits()) {
+                            blog = new Blog();
+                            blog.setId(Long.parseLong(hit.getSourceAsMap().get("id").toString()));
+                            blog.setContent(hit.getSourceAsMap().get("content").toString());
+                            blog.setDate(hit.getSourceAsMap().get("date").toString());
+                            HighlightField title = hit.getHighlightFields().get("title");
+                            if (null != title) {
+                                blog.setTitle(title.fragments()[0].toString());
+                            }
+                            blogs.add(blog);
+                        }
 
-						if (blogs.size() > 0) {
-							return new AggregatedPageImpl<>((List<T>) blogs);
-						}
+                        if (blogs.size() > 0) {
+                            return new AggregatedPageImpl<>((List<T>) blogs);
+                        }
 
-						return null;
-					}
-				});
-		LOG.info("总数=========={}", elasticsearchTemplate.count(query));
-		return aggregatedPage.getContent();
-	}
+                        return null;
+                    }
+                });
+        LOG.info("总数=========={}", elasticsearchTemplate.count(query));
+        return aggregatedPage.getContent();
+    }
 
 }
--- src/main/resources/static/js/blog.cmn.js
+++ src/main/resources/static/js/blog.cmn.js
@@ -1,48 +1,59 @@
-$(document).ready(function () {
-    $("#mnavh").click(function () {
-        $("#starlist").toggle();
-        $("#mnavh").toggleClass("open")
-    });
-    var new_scroll_position = 0;
-    var last_scroll_position;
-    var header = document.getElementById("header");
-    window.addEventListener("scroll", function (e) {
-        last_scroll_position = window.scrollY;
-        if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
-            header.classList.remove("slideDown");
-            header.classList.add("slideUp")
-        } else {
-            if (new_scroll_position > last_scroll_position) {
-                header.classList.remove("slideUp");
-                header.classList.add("slideDown")
-            }
-        }
-        new_scroll_position = last_scroll_position
-    });
-    var offset = 300, offset_opacity = 1200, scroll_top_duration = 700, $back_to_top = $(".cd-top");
-    $(window).scroll(function () {
-        ($(this).scrollTop() > offset) ? $back_to_top.addClass("cd-is-visible") : $back_to_top.removeClass("cd-is-visible cd-fade-out");
-        if ($(this).scrollTop() > offset_opacity) {
-            $back_to_top.addClass("cd-fade-out")
-        }
-    });
-    $back_to_top.on("click", function (event) {
-        event.preventDefault();
-        $("body,html").animate({scrollTop: 0,}, scroll_top_duration)
-    });
-    var Sticky = new hcSticky("aside", {stickTo: "main", innerTop: 200, followScroll: false})
-
-    function getUrlParam(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) {
-            return decodeURI(r[2]);
-        }
-        return null;
-    }
-
-    var option = getUrlParam('type');
-    console.log('type:' + option);
-    $('#starlist a').removeClass("active");
-    $('#starlist a').eq(option).addClass("active");
+// $(document).ready(function () {
+$("#mnavh").click(function () {
+    $("#starlist").toggle();
+    $("#mnavh").toggleClass("open")
 });
+
+var new_scroll_position = 0;
+var last_scroll_position;
+var header = $("#header");
+window.addEventListener("scroll", function (e) {
+    last_scroll_position = window.scrollY;
+    if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+        header.removeClass("slideDown");
+        header.addClass("slideUp")
+    } else {
+        if (new_scroll_position > last_scroll_position) {
+            header.removeClass("slideUp");
+            header.addClass("slideDown")
+        }
+    }
+    new_scroll_position = last_scroll_position
+});
+
+var offset = 300, offset_opacity = 1200, scroll_top_duration = 700, $back_to_top = $(".cd-top");
+$(window).scroll(function () {
+    ($(this).scrollTop() > offset) ? $back_to_top.addClass("cd-is-visible") : $back_to_top.removeClass("cd-is-visible cd-fade-out");
+    if ($(this).scrollTop() > offset_opacity) {
+        $back_to_top.addClass("cd-fade-out")
+    }
+});
+
+$back_to_top.on("click", function (event) {
+    event.preventDefault();
+    $("body,html").animate({scrollTop: 0,}, scroll_top_duration)
+});
+
+//¹Ì¶¨²àÀ¸
+new hcSticky("aside", {stickTo: "main", innerTop: 200, followScroll: false})
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURI(r[2]);
+    }
+    return null;
+}
+
+var option = getUrlParam('type');
+console.log('type:' + option);
+$('#starlist a').removeClass("active");
+$('#starlist a').eq(option).addClass("active");
+
+//Êý×Ö¹ö¶¯
+new CountUp('count1', 0, 71, 0, 2, {useGrouping: false}).start();
+new CountUp('count2', 0, 20, 0, 2, {useGrouping: false}).start();
+new CountUp('count3', 0, 520, 0, 2, {useGrouping: false}).start();
+new CountUp('count4', 0, 31, 0, 2, {useGrouping: false}).start();
+// });
